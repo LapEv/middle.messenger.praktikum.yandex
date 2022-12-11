64a7bdce733c7d24fec576a95565b4b37dcd7ea0
Evgeniy <51084295+LapEv@users.noreply.github.com>
2022-12-11 17:28:34 +0300

    correct server.js

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const hbs = require('hbs');
+const exphbs = require('express-handlebars');
 
 const PORT = 3000;
 
+app.engine(
+  'hbs',
+  exphbs.engine({
+    layoutsDir: path.join(__dirname, '/src/layouts'),
+    defaultLayout: 'auth',
+    extname: 'hbs',
+    partialsDir: [path.join(__dirname, '/src/partials')],
+  })
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/src/pages/'));
+// exphbs.registerPartials(path.join(__dirname, '/src/partials/'));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
+});
+
+app.get('/index', (req, res) => {
+  res.render('index.hbs');
+});
+
+app.get('/registration', (req, res) => {
+  res.render('registration.hbs');
 });
 
 app.listen(PORT, () => {
