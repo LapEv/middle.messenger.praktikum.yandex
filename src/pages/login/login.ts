193fb70aec8e083987021353734740adc746efa4
Evgeniy <51084295+LapEv@users.noreply.github.com>
2022-12-21 23:41:33 +0300

    in the process of refactoring

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,40 +1,58 @@
 import Block from '../../core/Block';
 import compiledTemplate from './login.hbs';
+import Validator, { ValidateRules } from '../../core/validator';
+import Form, { Props as FormProps } from '../../components/auth/authForm';
+import loginData from './loginData';
 // import './login.scss';
+
+type Props = {
+  authForm: FormProps;
+};
+
+const validateRules: ValidateRules = {
+  login: [
+    {
+      rule: (value) => value.length > 0,
+      errorMessage: 'Обязательное поле',
+    },
+    {
+      rule: Validator.defaultRegexp.name,
+      errorMessage: 'Логин содержит посторонние символы',
+    },
+  ],
+  password: [
+    {
+      rule: (value) => value.length > 0,
+      errorMessage: 'Обязательное поле',
+    },
+    {
+      rule: Validator.defaultRegexp.password,
+      errorMessage: 'Слишком простой',
+    },
+  ],
+};
+
+const validator: Validator = new Validator(validateRules);
 
 class Login extends Block {
   constructor(props: Props) {
     super(props, {
-      form: {
+      authForm: {
         component: Form,
         getProps: (props: Props) => ({ ...props.form, validator }),
       },
     });
   }
 
-  protected getStateFromProps() {
-    this.state = {
-      handleSubmit: (values: { login: string; password: string }) => {
-        console.log('submit', values);
-      },
-    };
-  }
-
   render() {
-    // const inputs = [{ name: 'login' }, { name: 'password' }];
-
-    // const link = {
-    //   to: '/signup',
-    //   label: 'Нет аккаунта?',
-    // };
-
-    // return `
-    //   <div>Login</div>
-    // `;
     const context = this.createCompileContext();
-    console.log('constext = ', context);
     return compiledTemplate(context);
   }
 }
+
+const login = new Login(loginData);
+
+const app = document.getElementById('app') as HTMLElement;
+app.append(login.getOuterElement());
 
 export default Login;
