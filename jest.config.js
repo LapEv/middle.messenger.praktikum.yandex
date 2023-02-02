1053827a8d32a70703bf736bf36a4437a6e9a129
Evgeniy <51084295+LapEv@users.noreply.github.com>
2023-02-02 17:43:12 +0300

    jest setup

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transformIgnorePatterns: [`/node_modules/(?!nanoid)`],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^nanoid(/(.*)|$)': 'nanoid$1',
+    // '^config/(.*)$': '<rootDir>/src/config/$1',
+    // '^components/(.*)$': '<rootDir>/src/components/$1',
+    '^core/(.*)$': '<rootDir>/src/core/$1',
+    // '^utils/(.*)$': '<rootDir>/src/utils/$1',
+    // '^pages/(.*)$': '<rootDir>/src/pages/$1',
+  },
+  clearMocks: true,
+};
